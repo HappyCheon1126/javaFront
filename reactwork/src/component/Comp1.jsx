122ef69d48e262ed59b9022343a4727eb86bece8
Happy Cheon <hzone@kakao.com>
2023-10-24 18:27:32 +0900

    20231024_1827

---
+++ reactwork/src/component/Comp1.jsx
@@ -0,0 +1,61 @@
+import { student, students } from "../data/data";
+import Student from "./Student";
+
+const Comp1 = () => {
+  /*
+  const student = {
+    name : '홍길동',
+    age  : 25,
+    address : '청주'
+  }
+
+  const students = [
+    {
+      name : '홍길동',
+      age  : 25,
+      address : '청주'
+    },
+    {
+      name : '김말숙',
+      age  : 30,
+      address : '서울'
+    },
+    {
+      name : '이기자',
+      age  : 19,
+      address : '제주'
+    }
+  ];
+  */
+
+  return (
+    <div className="comp1">
+      <h3>이곳은 Compd1입니다.</h3>
+      {/* 1개 값만 가져오기 */}
+      <p>
+        {student.name} / {student.age} / {student.address}
+      </p>
+      {/* 여러개의 값(배열)을 가져오기 */}
+      {/* 
+      <span>
+        {
+          students.map( s => (
+            <span>{s.name} / {s.age} / {s.address}<br/></span>
+          ))
+        }
+      </span>
+      */}
+      <hr/>
+
+      {/* 1개 자료 출력 */}
+      <Student stu={student}/>
+
+      {/* 여러개 자료 출력 */}
+      {students.map( s => (
+        <div>{s.name} / {s.age} / {s.address}</div>
+      ))}
+    </div>
+  );
+}
+
+export default Comp1;
---
+++ reactwork/src/component/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h2>이곳은 Home입니다.</h2>
+    </div>
+  );
+}
+
+export default Home;
